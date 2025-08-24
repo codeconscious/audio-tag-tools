1706c0fc2b085dc144eaf8713282faf58ac3b5b6
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-08-24 16:45:30 +0900

    Add opus extension

--- src/AudioTagTools.Cacher/IO.fs
+++ src/AudioTagTools.Cacher/IO.fs
@@ -14,10 +14,11 @@ let readfile filePath : Result<string, Error> =
 
 let getFileInfos (dirPath: DirectoryInfo) : Result<FileInfo seq, Error> =
     let isSupportedAudioFile (fileInfo: FileInfo) =
-        // Supported file format extensions from https://github.com/mono/taglib-sharp.
-        // The initial periods are needed.
+        // Supported file format extensions from https://github.com/mono/taglib-sharp,
+        // plus some additional ones. Initial periods are needed.
         [".aa"; ".aax"; ".aac"; ".aiff"; ".ape"; ".dsf"; ".flac"; ".m4a"; ".m4b"; "m4p"
-         ".mp3"; ".mp4"; ".mpc"; ".mpp"; ".ogg"; ".oga"; ".wav"; ".wma"; ".wv"; ".webm"]
+         ".mp3"; ".mpc"; ".mpp"; ".ogg"; ".oga"; ".wav"; ".wma"; ".wv"; ".webm"
+         ".mp4"; ".opus"; ] // This line contains additional custom ones.
         |> List.contains (fileInfo.Extension.ToLowerInvariant())
 
     try
