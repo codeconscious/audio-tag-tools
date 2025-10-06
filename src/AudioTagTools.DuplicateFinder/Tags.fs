4b44cbf1bd7327d6ea319c2405901ed92dc6bf78
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-10-06 20:58:26 +0900

    Fix spacing in summary

--- src/AudioTagTools.DuplicateFinder/Tags.fs
+++ src/AudioTagTools.DuplicateFinder/Tags.fs
@@ -109,10 +109,10 @@ let findDuplicates (settings: SettingsRoot) (tags: LibraryTags array) : LibraryT
     |> Option.map sortByArtist
 
 let printTotalCount (tags: LibraryTags array) =
-    printfn $"Total file count:    %s{formatInt tags.Length}"
+    printfn $"  Total file count:    %s{formatInt tags.Length}"
 
 let printFilteredCount (tags: LibraryTags array) =
-    printfn $"Filtered file count: %s{formatInt tags.Length}"
+    printfn $"  Filtered file count: %s{formatInt tags.Length}"
 
 let printResults (groupedTracks: LibraryTags array array option) =
     let printGroup index (groupTracks: LibraryTags array) =
