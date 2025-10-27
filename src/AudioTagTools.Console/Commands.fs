8c8d3eb6accd6fc7da3c76388df0c8c9ae46f364
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-10-27 17:33:12 +0900

    Change command name

--- src/AudioTagTools.Console/Commands.fs
+++ src/AudioTagTools.Console/Commands.fs
@@ -4,7 +4,7 @@ type Command = string array -> Result<string, string>
 
 let commandMap =
     [ "cache-tags", Cacher.Library.start
-      "analysis", LibraryAnalysis.Library.start
+      "analyze-library", LibraryAnalysis.Library.start
       "find-duplicates", DuplicateFinder.Library.start
       "export-genres", GenreExtractor.Library.start ]
     |> Map.ofList
