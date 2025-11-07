61dc4a23004acc9346c88d7d52ce027f8a53ef73
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-11-07 15:06:00 +0900

    Add space

--- src/AudioTagTools.Cacher/IO.fs
+++ src/AudioTagTools.Cacher/IO.fs
@@ -16,9 +16,9 @@ let getFileInfos (dirPath: DirectoryInfo) : Result<FileInfo seq, Error> =
     let isSupportedAudioFile (fileInfo: FileInfo) =
         // Supported file format extensions from https://github.com/mono/taglib-sharp,
         // plus some additional ones. Initial periods are needed.
-        [".aa"; ".aax"; ".aac"; ".aiff"; ".ape"; ".dsf"; ".flac"; ".m4a"; ".m4b"; "m4p"
-         ".mp3"; ".mpc"; ".mpp"; ".ogg"; ".oga"; ".wav"; ".wma"; ".wv"; ".webm"
-         ".mp4"; ".opus"; ] // This line contains additional custom ones.
+        [ ".aa"; ".aax"; ".aac"; ".aiff"; ".ape"; ".dsf"; ".flac"; ".m4a"; ".m4b"; "m4p"
+          ".mp3"; ".mpc"; ".mpp"; ".ogg"; ".oga"; ".wav"; ".wma"; ".wv"; ".webm"
+          ".mp4"; ".opus" ] // This line contains additional custom ones.
         |> List.contains (fileInfo.Extension.ToLowerInvariant())
 
     try
