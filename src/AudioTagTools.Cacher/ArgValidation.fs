500ec677e7b6e0cd03e6a799b66f54682ac83d81
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-01-31 10:19:53 +0900

    Add dir-exists validation

--- src/AudioTagTools.Cacher/ArgValidation.fs
+++ src/AudioTagTools.Cacher/ArgValidation.fs
@@ -6,6 +6,8 @@ open System.IO
 let validate (args: string array) : Result<DirectoryInfo * FileInfo, Error> =
     match args with
     | [| mediaDirPath; tagLibraryPath |] ->
-        Ok (DirectoryInfo mediaDirPath, FileInfo tagLibraryPath)
+        match Directory.Exists mediaDirPath with
+        | false -> Error (MediaDirectoryMissing mediaDirPath)
+        | true  -> Ok (DirectoryInfo mediaDirPath, FileInfo tagLibraryPath)
     | _ ->
         Error InvalidArgCount
