14f62b1d981b2cb6d9a838c5f913b1092c9a81d6
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-02 22:07:57 +0900

    Update genre-extraction validation

--- src/AudioTagTools.GenreExtractor/ArgValidation.fs
+++ src/AudioTagTools.GenreExtractor/ArgValidation.fs
@@ -1,11 +1,34 @@
 module GenreExtractor.ArgValidation
 
 open Errors
+open CCFSharpUtils.Library
+open FSharpPlus
+open FSharpPlus.Data
+open FsToolkit.ErrorHandling
 open System.IO
 
-let validate (args: string array) : Result<FileInfo * FileInfo, Error> =
+let private validationToResult (v: Validation<'a, string>) : Result<'a, Error> =
+    match v with
+    | Ok ok ->
+        Ok ok
+    | Error errs ->
+        errs
+        |> NonEmptyList.ofList
+        |> IoFilesMissing
+        |> Error
+
+let validate (args: string array) : Result<(FileInfo * FileInfo), Error> =
+    let verifyExists err file =
+        if File.Exists file
+        then Ok file
+        else Error [err]
+
     match args with
-    | [| tagLibraryPath; genreFilePath |] ->
-        Ok (FileInfo tagLibraryPath, FileInfo genreFilePath)
+    | [| tagLibArg; genreFileArg |] ->
+        Validation.map2
+            (fun settingsFile tagLib -> FileInfo settingsFile, FileInfo tagLib)
+            (tagLibArg    |> verifyExists $"Tag library file \"{tagLibArg}\" does not exist.")
+            (genreFileArg |> verifyExists $"Genre file \"{genreFileArg}\" does not exist.")
+        |> validationToResult
     | _ ->
-        Error InvalidArgCount
+        Error ArgCountError
