c183a3a25fa81f4a0f6b5d542b5bd0ad8e4bd626
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-18 15:35:23 +0900

    Add operator to dupe-finding

--- src/AudioTagTools.DuplicateFinder/ArgValidation.fs
+++ src/AudioTagTools.DuplicateFinder/ArgValidation.fs
@@ -7,6 +7,11 @@ open FSharpPlus
 open FSharpPlus.Data
 open System.IO
 
+let (||!) (v: Validation<'err1, 'ok>) (f: 'err1 -> 'err2) : Result<'ok, 'err2> =
+    v
+    |> Validation.toResult
+    |> Result.mapError f
+
 let validate args : Result<(FileInfo * FileInfo), Error> =
     match args with
     | [| settingsArg; tagLibArg |] ->
@@ -15,7 +20,6 @@ let validate args : Result<(FileInfo * FileInfo), Error> =
             and! tagLibFile = tagLibArg |> toFileInfo $"Tag library file \"{tagLibArg}\" does not exist."
             return (settingsFile, tagLibFile)
         }
-        |> Validation.toResult
-        |! (NonEmptyList.ofList >> ArgErrors)
+        ||! (NonEmptyList.ofList >> ArgErrors)
     | _ ->
         Error ArgCountError
