078d96499ab6cd191605e6751607dac96258e889
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-04-10 18:07:48 +0900

    Restore collection module names

--- src/AudioTagTools.DuplicateFinder/Tags.fs
+++ src/AudioTagTools.DuplicateFinder/Tags.fs
@@ -81,12 +81,12 @@ let findDuplicates settings (tags: LibraryTags NonEmptyList)
     : LibraryTags NonEmptyList NonEmptyList option =
 
     tags
-    |> toList
-    |> filter hasArtistAndTitle
-    |> groupBy (groupName settings)
-    |> filter (snd >> List.hasMultiple)
-    |> sortBy fst // Group name
-    |> map (snd >> sortBy (mainArtists String.Empty) >> NonEmptyList.ofList)
+    |> NonEmptyList.toList
+    |> List.filter hasArtistAndTitle
+    |> List.groupBy (groupName settings)
+    |> List.filter (snd >> List.hasMultiple)
+    |> List.sortBy fst // Group name
+    |> List.map (snd >> sortBy (mainArtists String.Empty) >> NonEmptyList.ofList)
     |> List.toNonEmptyListOption
 
 let printDuplicates (groupedTracks: LibraryTags NonEmptyList NonEmptyList option) : unit =
