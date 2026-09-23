459864e7f6feb35dab5c73b32a72d191afeec5c4
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-09-23 17:53:08 +0900

    Refactor tryMainArtistName

--- src/AudioTagTools.Shared/TagLibrary.fs
+++ src/AudioTagTools.Shared/TagLibrary.fs
@@ -85,11 +85,9 @@ let isNotIgnoredArtist artist =
     not (List.exists ((=) artist) ignoredArtists)
 
 let tryMainArtistName tags : string option =
-    if   List.isNotEmpty tags.AlbumArtists
-    then Some (List.head tags.AlbumArtists)
-    elif List.isNotEmpty tags.Artists
-    then Some (List.head tags.Artists)
-    else None
+    tags.AlbumArtists
+    |> List.tryHead
+    |> Option.orElse (List.tryHead tags.Artists)
 
 let tryMainArtist tags : Artist option =
     tags |> tryMainArtistName |> Option.map Artist
